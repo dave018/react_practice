bb3c6dbf89b9b1efa312d4777bd18429e721fdbd
dave018 <sehwanjeon0316@gmail.com>
2024-03-12 19:39:37 +0900

    [Practice] Add base code for routing details of movies
    Signed-off-by: dave018 <sehwanjeon0316@gmail.com>

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,12 +1,17 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setLoading(false);
+    setMovie(json.data.movie);
     console.log(json);
   };
 
@@ -14,7 +19,24 @@ function Detail() {
     getMovie();
   }, []);
 
-  return <h1>Details of Movie</h1>;
+  return (
+    <div>
+      {loading ? (
+        <h1>Loading Details of Movies ...</h1>
+      ) : (
+        <h1>
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            title={movie.title}
+            coverImg={movie.medium_cover_image}
+            rating={movie.rating}
+            genres={movie.genres}
+          />
+        </h1>
+      )}
+    </div>
+  );
 }
 
 export default Detail;
